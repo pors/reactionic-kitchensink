d1b11bdf520a208704e7bf893b54e60f89564c1a
Mark Pors <mark@pors.net>
2016-03-29 19:01:22 +0300

    merge ionRange

--- app/client/imports/components/forms.jsx
+++ app/client/imports/components/forms.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonList, IonItem, IonItemCheckBox, IonItemToggle, IonItemRadio, IonSelect } from 'reactionic';
+import { IonContent, IonList, IonItem, IonItemCheckBox, IonItemRadio, IonSelect, IonIcon, IonItemToggle, IonRange } from 'reactionic';
 
 var Forms = React.createClass({
   getInitialState: function () {
@@ -7,14 +7,19 @@ var Forms = React.createClass({
       checkedRadio: 'Button',
       checkedBox: false,
       checkedToggle: false,
-      selectValue: 'yellow'
+      selectValue: 'yellow',
+      rangeValue: '33',
     }
   },
+
   radioSelection(name, value) {
     this.setState({ checkedRadio:value });
   },
   changeValue(value){
     this.setState({ selectValue: value});
+  },
+  rangeSelection(value){
+    this.setState({ rangeValue: value});
   },
   render() {
     var boxLabel    = 'Checkbox ' + (this.state.checkedBox ? 'checked!' : 'unchecked');
@@ -22,6 +27,8 @@ var Forms = React.createClass({
     var radioLabel  = 'Radio ' + this.state.checkedRadio;
     var selectLabel = 'Color: ' + this.state.selectValue;
     var selectOptions = ['blue','yellow','red'];
+    var rangeLabel  = 'Range '+ this.state.rangeValue;
+
     return (
       <IonContent customClasses=""
                   {...this.props}>
@@ -31,15 +38,13 @@ var Forms = React.createClass({
               color="positive"
               label={boxLabel}
               checked={false}
-              handleChange={ (toggle) => this.setState({ checkedBox:toggle }) }
-          />
+              handleChange={ (toggle) => this.setState({ checkedBox:toggle }) }/>
           <IonItem divider>Toggle</IonItem>
           <IonItemToggle
               color="positive"
               label={toggleLabel}
               checked={false}
-              handleChange={ (toggle) => this.setState({ checkedToggle:toggle }) }
-          />
+              handleChange={ (toggle) => this.setState({ checkedToggle:toggle }) }/>
           <IonItem divider>{radioLabel}</IonItem>
           <IonItemRadio
               name="group"
@@ -47,30 +52,35 @@ var Forms = React.createClass({
               value="Button"
               icon="checkmark"
               handleChange={this.radioSelection}
-              checked={this.state.checkedRadio === "Button"}
-          />
+              checked={this.state.checkedRadio === "Button"}/>
           <IonItemRadio
               name="group"
               label="Shack"
               value="Shack"
               icon="checkmark"
               handleChange={this.radioSelection}
-              checked={this.state.checkedRadio === "Shack"}
-          />
+              checked={this.state.checkedRadio === "Shack"}/>
           <IonItemRadio
               name="group"
               label="Gaga"
               value="Gaga"
               icon="checkmark"
               handleChange={this.radioSelection}
-              checked={this.state.checkedRadio === "Gaga"}
-          />
+              checked={this.state.checkedRadio === "Gaga"}/>
           <IonItem divider>Select</IonItem>
           <IonSelect  label={selectLabel}
                       options={selectOptions}
                       defaultValue='yellow'
                       handleChange={this.changeValue}>
           </IonSelect>
+          <IonItem divider>{rangeLabel}</IonItem>
+          <IonRange iconBeforeInput={<IonIcon icon='ios-sunny-outline'/>}
+                    iconAfterInput={<IonIcon icon='ios-sunny'/>}
+                    defaultValue={33}
+                    handleChange={this.rangeSelection}
+                    min={0}
+                    max={100}>
+          </IonRange>
         </IonList>
 
       </IonContent>
